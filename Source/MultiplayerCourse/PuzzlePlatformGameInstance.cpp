e8b9baed50261173b2a6b91a3f98703b60ce6611
Saulo Soares <35870646+saulo308@users.noreply.github.com>
2020-06-24 19:32:17 -0300

    LE17: GameInstance

---
+++ Source/MultiplayerCourse/PuzzlePlatformGameInstance.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PuzzlePlatformGameInstance.h"
+
+UPuzzlePlatformGameInstance::UPuzzlePlatformGameInstance() {
+	UE_LOG(LogTemp, Warning, TEXT("Construct!"));
+}
+
+void UPuzzlePlatformGameInstance::Init() {
+	UE_LOG(LogTemp, Warning, TEXT("Init!"));
+}
