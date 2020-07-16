20513e1828213cb7ab8424409515207d504a931a
Saulo Soares <35870646+saulo308@users.noreply.github.com>
2020-07-16 16:53:24 -0300

    LE72: Seamless travel

--- Source/MultiplayerCourse/LobbyGameMode.cpp
+++ Source/MultiplayerCourse/LobbyGameMode.cpp
@@ -4,16 +4,25 @@
 #include "LobbyGameMode.h"
 
 void ALobbyGameMode::PostLogin(APlayerController* NewPlayer) {
-	//Super::PostLogin(NewPlayer);
+	Super::PostLogin(NewPlayer);
 	UE_LOG(LogTemp, Warning, TEXT("Welcome new player!"));
 	++ConnectedPlayerNum;
 
-	if (ConnectedPlayerNum >= 3)
+	if (ConnectedPlayerNum >= 2) {
 		UE_LOG(LogTemp, Warning, TEXT("Has reached top player num!!!!"));
+
+		//Seamless travel
+		bUseSeamlessTravel = true;
+
+		//Travelling
+		auto World = GetWorld();
+		if (!World) return;
+		World->ServerTravel(TEXT("/Game/ThirdPersonCPP/Maps/ThirdPersonExampleMap?listen"));
+	}
 }
 
 void ALobbyGameMode::Logout(AController* Exiting) {
-	//Super::Logout(Exiting);
+	Super::Logout(Exiting);
 	UE_LOG(LogTemp, Warning, TEXT("Bye bye!"));
 	--ConnectedPlayerNum;
 }
